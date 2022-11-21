5653a07b32904af58e09914fed3b09b469d0c0f8
tsirito <tsiritodezavalia@itba.edu.ar>
2022-11-21 14:11:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,6 @@ function openWin(){
 
   function change() {
     document.getElementById("text").innerHTML = "Y que haces aca entonces? anda, se libre";
-    document.getElementById("imag").src = "https://www.google.com.ar/imgres?imgurl=https%3A%2F%2Fi.imgflip.com%2F4%2F8h0c8.jpg&imgrefurl=https%3A%2F%2Fimgflip.com%2Fmemesearch%3Fq%3Dconfused&tbnid=WATuOVhm6SwGdM&vet=12ahUKEwiQmsqGl777AhW1uZUCHfJlCLQQMygEegUIARC8AQ..i&docid=ULAbtUynYiVkhM&w=239&h=250&q=confused%20meme&hl=es&ved=2ahUKEwiQmsqGl777AhW1uZUCHfJlCLQQMygEegUIARC8AQ";
+    document.getElementById("imag").src = "red0\confused.jpg";
+    document.getElementById('butt').style.display='none';
   }
